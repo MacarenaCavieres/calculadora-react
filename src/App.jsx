bd7a9ac557895497457ef59ab5b7b211cec959c6
MCavieres <maca.c.91@gmail.com>
2024-05-27 18:24:09 -0400

    descarga de paquete mathjs y calculo de resultado

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,21 @@ import Boton from "./componentes/boton.jsx";
 import Pantalla from "./componentes/pantalla.jsx";
 import BotonClear from "./componentes/botonClear.jsx";
 import { useState } from "react";
+import { evaluate } from "mathjs";
 
 function App() {
     const [input, setInput] = useState("");
 
     const agregarInput = (val) => {
         setInput(input + val);
+    };
+
+    const calcularResultado = () => {
+        if (input) {
+            setInput(evaluate(input));
+        } else {
+            alert("Por favor ingresar valores");
+        }
     };
 
     return (
@@ -35,9 +44,9 @@ function App() {
                     <Boton manejarClick={agregarInput}>*</Boton>
                 </div>
                 <div className="fila">
-                    <Boton manejarClick={agregarInput}>=</Boton>
-                    <Boton manejarClick={agregarInput}>0</Boton>
                     <Boton manejarClick={agregarInput}>.</Boton>
+                    <Boton manejarClick={agregarInput}>0</Boton>
+                    <Boton manejarClick={calcularResultado}>=</Boton>
                     <Boton manejarClick={agregarInput}>/</Boton>
                 </div>
                 <div className="fila">
