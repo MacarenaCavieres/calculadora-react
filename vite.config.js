441cc57dffc9d4b157fe198f625e61bada54cbeb
MCavieres <maca.c.91@gmail.com>
2024-05-27 20:14:25 -0400

    config vite

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "https://macarenacavieres.github.io/calculadora-react",
+    base: "/calculadora-react/",
 });
